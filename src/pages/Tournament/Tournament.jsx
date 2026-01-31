ee39aa91aa5b3a6d46476a71793c8f915866dc9c
sabbirHossain <sabbirshnt@gmail.com>
2026-01-31 12:15:01 +0600

    done

--- src/pages/Tournament/Tournament.jsx
+++ src/pages/Tournament/Tournament.jsx
@@ -1,112 +1,61 @@
+import { useNavigate } from "react-router-dom";
+import { useGetIndex } from "../../hooks";
+
 const Tournament = () => {
+  const navigate = useNavigate();
+  const { data } = useGetIndex({
+    type: "wolf_tournaments",
+  });
+
+  const navigateGameList = (item) => {
+    navigate(`/event-details/${item?.event_type_id}/${item?.tournament_id}`);
+  };
+
   return (
-    <div className="page-body notranslate">
+    <div
+      className="page-body notranslate"
+      style={{
+        marginBottom: "0px",
+        height: "100%",
+        maxHeight: "100%",
+        overflowY: "hidden",
+      }}
+    >
       <div className="special-market ng-star-inserted">
         <div className="market-wrap ng-star-inserted">
           <div className="title-bar">
             <h2 className="title">Series Winner</h2>
           </div>
-          <div className="market-card ng-star-inserted">
-            <div className="img-wrap">
-              <img
-                alt="Overlay"
-                className="overlay-img"
-                src="https://ss.manage63.com/bmk-wl/commonAssets/frame.webp"
-              />
-              <img
-                alt=""
-                className="mkt-img"
-                src="https://cdnimg.manage63.com/cricflags/1768883378275_72164_QGPUXFVAZEVDEVGNZCJKKWJZZ.jpeg"
-              />
-            </div>
-            <div className="event-title">
-              <h2>India v New Zealand T20 series</h2>
-              <p className="ng-star-inserted">21 Jan 2026 to 31 Jan 2026</p>
-            </div>
-            <div className="img-wrap ng-star-inserted">
-              <img
-                alt="Overlay"
-                className="overlay-img"
-                src="https://ss.manage63.com/bmk-wl/commonAssets/frame.webp"
-              />
-              <img
-                alt=""
-                className="mkt-img"
-                src="https://cdnimg.manage63.com/cricflags/1768883389034_946546_FZZUGDWRJKTAZNWHBWDJYSMGN.jpeg"
-              />
-            </div>
-          </div>
-          <div className="market-card ng-star-inserted">
-            <div className="img-wrap">
-              <img
-                alt="Overlay"
-                className="overlay-img"
-                src="https://ss.manage63.com/bmk-wl/commonAssets/frame.webp"
-              />
-              <img
-                alt=""
-                className="mkt-img"
-                src="https://cdnimg.manage63.com/cricflags/1769054040144_797933_ZGRHDKMAGQNTGNZFMFGPNMDVR.jpeg"
-              />
-            </div>
-            <div className="event-title">
-              <h2>Sri Lanka v England ODI SERIES</h2>
-              <p className="ng-star-inserted">22 Jan to 27 Jan 2026</p>
-            </div>
-            <div className="img-wrap ng-star-inserted">
-              <img
-                alt="Overlay"
-                className="overlay-img"
-                src="https://ss.manage63.com/bmk-wl/commonAssets/frame.webp"
-              />
-              <img
-                alt=""
-                className="mkt-img"
-                src="https://cdnimg.manage63.com/cricflags/1769054050747_348687_EXUSSUHJYJHNAAWMKSXQQYJTX.jpeg"
-              />
-            </div>
-          </div>
-          <div className="market-card ng-star-inserted">
-            <div className="img-wrap">
-              <img
-                alt="Overlay"
-                className="overlay-img"
-                src="https://ss.manage63.com/bmk-wl/commonAssets/frame.webp"
-              />
-              <img
-                alt=""
-                className="mkt-img"
-                src="../assets/img/default-img.svg"
-              />
-            </div>
-            <div className="event-title">
-              <h2>South Africa v West Indies T20 SERIES</h2>
-              <p className="ng-star-inserted">27 Jan 2026 to 31 Jan 2026</p>
-            </div>
-          </div>
-        </div>
-        <div className="market-wrap ng-star-inserted">
-          <div className="title-bar">
-            <h2 className="title">Tournament</h2>
-          </div>
-          <div className="market-card ng-star-inserted">
-            <div className="img-wrap">
-              <img
-                alt="Overlay"
-                className="overlay-img"
-                src="https://ss.manage63.com/bmk-wl/commonAssets/frame.webp"
-              />
-              <img
-                alt=""
-                className="mkt-img"
-                src="https://cdnimg.manage63.com/cricflags/1767588277981_163793_FWNYFZPYQNPSPVBEWWYSKJTAC.png"
-              />
-            </div>
-            <div className="event-title">
-              <h2>Womens Premier League 2026</h2>
-              <p className="ng-star-inserted">9 Jan 2026 to 5 Feb 2026</p>
-            </div>
-          </div>
+          {data?.data?.map((item) => {
+            return (
+              <div
+                onClick={() => navigateGameList(item)}
+                key={item?.tournament_id}
+                className="market-card ng-star-inserted"
+              >
+                <div className="img-wrap">
+                  <img
+                    alt="Overlay"
+                    className="overlay-img"
+                    src="https://ss.manage63.com/bmk-wl/commonAssets/frame.webp"
+                  />
+                  <img alt="" className="mkt-img" src={item?.event_icon} />
+                </div>
+                <div className="event-title">
+                  <h2>{item?.fixture_name}</h2>
+                  <p className="ng-star-inserted">{item?.start_end_date}</p>
+                </div>
+                <div className="img-wrap ng-star-inserted">
+                  <img
+                    alt="Overlay"
+                    className="overlay-img"
+                    src="https://ss.manage63.com/bmk-wl/commonAssets/frame.webp"
+                  />
+                  <img alt="" className="mkt-img" src={item?.event_icon1} />
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
