6b9abe9d174471bde62020d03a2e1be0a7c399cc
sabbirHossain <sabbirshnt@gmail.com>
2026-03-28 14:12:51 +0600

    done

--- src/components/modals/BetSlip/BetSlip.jsx
+++ src/components/modals/BetSlip/BetSlip.jsx
@@ -118,30 +118,39 @@ const BetSlip = () => {
         ...payload,
 
         nounce: uuidv4(),
-        isbetDelay: Settings?.bet_delay,
+        isbetDelay:
+          placeBetValues?.btype === "FANCY" &&
+          placeBetValues?.eventTypeId === "4"
+            ? false
+            : Settings.bet_delay,
         apk: closePopupForForever ? true : false,
       },
     ];
     setLoading(true);
     let delay = 0;
     if (
-      (eventTypeId == 4 || eventTypeId == 2) &&
-      placeBetValues?.btype === "MATCH_ODDS" &&
-      price > 3 &&
-      placeBetValues?.name?.length === 2
+      placeBetValues?.btype !== "FANCY" &&
+      placeBetValues?.eventTypeId !== "4"
     ) {
-      delay = 9000;
-    }
-    if (
-      (eventTypeId == 4 || eventTypeId == 2) &&
-      placeBetValues?.btype === "MATCH_ODDS" &&
-      price > 7 &&
-      placeBetValues?.name?.length === 3
-    ) {
-      delay = 9000;
-    } else {
-      setBetDelay(currentPlaceBetEvent?.betDelay);
-      delay = Settings?.bet_delay ? currentPlaceBetEvent?.betDelay * 1000 : 0;
+      if (
+        (eventTypeId == 4 || eventTypeId == 2) &&
+        placeBetValues?.btype === "MATCH_ODDS" &&
+        price > 3 &&
+        placeBetValues?.name?.length === 2
+      ) {
+        delay = 9000;
+      }
+      if (
+        (eventTypeId == 4 || eventTypeId == 2) &&
+        placeBetValues?.btype === "MATCH_ODDS" &&
+        price > 7 &&
+        placeBetValues?.name?.length === 3
+      ) {
+        delay = 9000;
+      } else {
+        setBetDelay(currentPlaceBetEvent?.betDelay);
+        delay = Settings?.bet_delay ? currentPlaceBetEvent?.betDelay * 1000 : 0;
+      }
     }
 
     setTimeout(async () => {
