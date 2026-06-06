95b0d3efd1ca6030c57c623acadb69e4342efd0c
sabbirHossain <sabbirshnt@gmail.com>
2026-06-06 10:51:20 +0600

    done

--- src/hooks/depositBreakdown.js
+++ src/hooks/depositBreakdown.js
@@ -2,10 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import { API } from "../api";
 import { AxiosSecure } from "../lib/AxiosSecure";
 
-export const useDepositBreakdownQuery = () => {
+export const useDepositBreakdownQuery = (payload) => {
   return useQuery({
     queryKey: ["deposit-breakdown"],
-    queryFn: async (payload) => {
+    queryFn: async () => {
       const { data } = await AxiosSecure.post(
         `${API.depositBreakdown}`,
         payload,
